c5163e2c5e4334c45ff8b338d08d3772b23c7674
Mitko Mitev <onesible@gmail.com>
2025-03-13 18:43:18 +0200

    added more solutions

--- 04-Objects-and-Classes/excercises/02-towns.js
+++ 04-Objects-and-Classes/excercises/02-towns.js
@@ -2,6 +2,6 @@ function solve(arr) {
     for (let town of arr) {
         let [city, latitude, longitude] = town.split(' | ');
 
-        console.log({ town: city, latitude: Number(latitude).toFixed(2), longitude: Number(longitude).toFixed(2)} )
+        console.log({town: city, latitude: Number(latitude).toFixed(2), longitude: Number(longitude).toFixed(2)})
     }
 }
