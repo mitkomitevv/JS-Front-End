e29f30df9155b638fe114ac01c730f591994db23
Mitko Mitev <onesible@gmail.com>
2025-03-25 17:52:15 +0200

    added more solutions

--- 06-DOM-Manipulation-and-Events/06. Dynamic-Validation/dynamic-validation.js
+++ 06-DOM-Manipulation-and-Events/06. Dynamic-Validation/dynamic-validation.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    let input = document.querySelector('[type="text"]');
+    input.addEventListener('change', validateEmail);
+
+    const emailRegex = /^[a-z]+@[a-z]+\.[a-z]{2,}$/;
+
+    function validateEmail(event) {
+        if (!event.target.value || emailRegex.test(event.target.value)) {
+            event.target.classList.remove('error');
+        } else {
+            event.target.classList.add('error');
+        }
+    }
 }
--- 06-DOM-Manipulation-and-Events/07. Shopping-Cart/shopping-cart.js
+++ 06-DOM-Manipulation-and-Events/07. Shopping-Cart/shopping-cart.js
@@ -1,5 +1,40 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-   //TODO
+   let result = {};
+   let addButton = Array.from(document.querySelectorAll('.add-product'));
+   addButton.forEach(e => e.addEventListener('click', addToCart));
+
+   let textArea = document.querySelector('textarea')
+
+   function addToCart(event) {
+      let productContainer = event.target.parentElement.parentElement;
+      let productTitle = productContainer.querySelector('.product-title').textContent;
+      let productPrice = productContainer.querySelector('.product-line-price').textContent;
+
+      textArea.textContent += `Added ${productTitle} for ${Number(productPrice).toFixed(2)} to the cart.\n`
+
+      if (result.hasOwnProperty(productTitle)) {
+         result[productTitle] += Number(productPrice);
+         return;
+      }
+      result[productTitle] = Number(productPrice);
+   }
+
+   document.querySelector('.checkout').addEventListener('click', checkOut);
+
+   function checkOut() {
+      let productNames = [];
+      let totalPrice = 0;
+
+      for (let product in result) {
+         productNames.push(product);
+         totalPrice += Number(result[product]);
+      }
+
+      textArea.textContent += `You bought ${productNames.join(', ')} for ${totalPrice.toFixed(2)}.`
+
+      document.querySelector('.checkout').removeEventListener('click', checkOut);
+      addButton.forEach(e => e.removeEventListener('click', addToCart));
+   }
 }
